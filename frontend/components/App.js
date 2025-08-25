e2dee7e95bdb4e4fd217793834f0f0b07d2c9a38
jaacob34 <jacobhernandez34@icloud.com>
2025-08-24 17:58:56 -0700

    completed everything just need to add default in app.js

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react' // 👈 you'll need the reducer hook
+import React, { useReducer } from 'react' // 👈 you'll need the reducer hook
 import Quotes from './Quotes'
 import QuoteForm from './QuoteForm'
 
@@ -33,36 +33,89 @@ const quotes = [
 ]
 
 // 👇 create your initial state object here
+const initialState = {
+  displayAllQuotes: true,
+  highlightedQuote: null,
+  quotes: quotes
+}
 
 const reducer = (state, action) => {
   // 👇 implement your reducer here using the action types above
+  switch (action.type) {
+    case CREATE_QUOTE:
+      return { 
+        ...state,
+        quotes: [...state.quotes, action.payload]
+      }
+    case DELETE_QUOTE:
+      return { 
+        ...state,
+        quotes: state.quotes.filter(q => q.id !== action.payload)
+      }
+    case EDIT_QUOTE_AUTHENTICITY:
+      return {
+        ...state,
+        quotes: state.quotes.map(q => 
+          q.id === action.payload ? { ...q, apocryphal: !q.apocryphal } : q
+        )
+      }
+    case SET_HIGHLIGHTED_QUOTE:
+      return {
+        ...state,
+        highlightedQuote: state.highlightedQuote === action.payload
+          ? null
+          : action.payload
+      }
+    case TOGGLE_VISIBILITY:
+      return {
+        ...state,
+        displayAllQuotes: !state.displayAllQuotes,
+      }
+  }
 }
 
 export default function App() {
   // 👇 use the reducer hook to spin up state and dispatch
+  const [state, dispatch] = useReducer(reducer, initialState)
 
   const createQuote = ({ authorName, quoteText }) => {
     // 👇 use the helper function above to create a new quote
     // 👇 and dispatch it over to the reducer
+    dispatch({ type: CREATE_QUOTE, payload: {
+      id: getNextId(),
+      authorName: authorName,
+      quoteText: quoteText,
+      apocryphal: false
+    } })
   }
   const deleteQuote = id => {
     // 👇 implement
+    dispatch({ type: DELETE_QUOTE, payload: id })
   }
   const editQuoteAuthenticity = id => {
     // 👇 implement
+    dispatch({ type: EDIT_QUOTE_AUTHENTICITY, payload: id })
   }
   const setHighlightedQuote = id => {
     // 👇 implement
+    dispatch({ type: SET_HIGHLIGHTED_QUOTE, payload: id })
   }
   const toggleVisibility = () => {
     // 👇 implement
+    dispatch({ type: TOGGLE_VISIBILITY })
   }
 
   return (
     <div id="mp">
       <h2>Module Project</h2>
       <Quotes
-        quotes={quotes}
+        quotes={state.quotes}
+        deleteQuote={deleteQuote}
+        editQuoteAuthenticity={editQuoteAuthenticity}
+        highlightedQuote={state.highlightedQuote}
+        setHighlightedQuote={setHighlightedQuote}
+        toggleVisibility={toggleVisibility}
+        displayAllQuotes={state.displayAllQuotes}
       // 👇 lots of props are missing! Check the Quotes component
 
       />
--- frontend/components/QuoteForm.js
+++ frontend/components/QuoteForm.js
@@ -1,25 +1,45 @@
-import React from 'react' // 👈 you'll need the reducer hook
+import React, { useReducer } from 'react' // 👈 you'll need the reducer hook
 
 // 👇 these are the types of actions that can change state
 const CHANGE_INPUT = 'CHANGE_INPUT'
 const RESET_FORM = 'RESET_FORM'
 
 // 👇 create your initial state object here
+const initialState = {
+  authorName: '',
+  quoteText: ''
+} 
 
 // 👇 create your reducer function here
+const reducer = (state, action) => {
+  switch (action.type) {
+    case CHANGE_INPUT: {
+      const { name, value } = action.payload
+      return { ...state, [name]: value }
+    }
+    case RESET_FORM:
+      return initialState
+    default:
+      return state
+  }
+}
 
 export default function TodoForm({ createQuote = () => { } }) {
   // 👇 use the reducer hook to spin up state and dispatch
+  const [state, dispatch] = useReducer(reducer, initialState)
 
-  const onChange = () => {
+  const onChange = (e) => {
     // 👇 implement
+    const { name, value } = e.target
+    dispatch({ type: CHANGE_INPUT, payload: { name: name, value: value } })
   }
-  const resetForm = () => {
+  const resetForm = () => dispatch({ type: RESET_FORM })
+
+  const onNewQuote = (e) => {
     // 👇 implement
-  }
-  const onNewQuote = () => {
-    // 👇 implement
+    e.preventDefault()
     resetForm()
+    createQuote(state)
   }
 
   // 👇 some props are missing in the JSX below:
@@ -32,6 +52,7 @@ export default function TodoForm({ createQuote = () => { } }) {
           name='authorName'
           placeholder='type author name'
           onChange={onChange}
+          value={state.authorName}
         />
       </label>
       <label><span>Quote text:</span>
@@ -40,11 +61,12 @@ export default function TodoForm({ createQuote = () => { } }) {
           name='quoteText'
           placeholder='type quote'
           onChange={onChange}
+          value={state.quoteText}
         />
       </label>
       <label><span>Create quote:</span>
         <button
-          role='submit'
+          type='submit'
         >DO IT!</button>
       </label>
     </form>
